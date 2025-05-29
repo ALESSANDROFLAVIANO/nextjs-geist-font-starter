271d6d4a1453cacf4acb0a4770f47546eac55ab4
ALESSANDROFLAVIANO <flaviano.tvcine@gmail.com>
2025-05-28 20:26:52 -0400

    Add .next/static/chunks/src_app_page_tsx_c0237562._.js 1748478411746

---
+++ .next/static/chunks/src_app_page_tsx_c0237562._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_988de36d._.js",
+  "static/chunks/src_3c534d60._.js"
+],
+    source: "dynamic"
+});
